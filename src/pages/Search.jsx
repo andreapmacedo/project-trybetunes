3d833d1fd1fddc920bf22b084882ca85864240fd
Andre <aa.andremacedo@gmail.com>
2022-04-21 09:16:36 -0300

    adiciona requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,23 +1,100 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Fragment } from 'react/cjs/react.production.min';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       artist: '',
+      // loading: false,
+      albumsResult: [],
+      searchResults: false,
+      artistSearched: '',
     };
   }
 
   inputHandleChange = ({ target }) => {
-    const { value, name } = target;
-    this.setState({ [name]: value });
+    const { value } = target;
+    this.setState({ artist: value });
   }
 
-  render() {
+  searchArtists = async () => {
     const { artist } = this.state;
+    const albums = await searchAlbumsAPI(artist);
+    console.log(albums);
+    this.setState(
+      // { loading: true },
+      async () => {
+        this.setState({
+          artist: '',
+          // loading: false,
+          albumsResult: [...albums],
+          searchResults: true,
+          artistSearched: artist,
+        });
+      },
+    );
+  }
+
+  renderAlbums = () => {
+    const { albumsResult } = this.state;
+    // console.log(albumsResult);
+    // albumsResult.map((album) => console.log(album.artistName));
+    // if (albumsResult.length === 0) return <p>Nenhum álbum foi encontrado</p>;
+    return albumsResult.map((album) => (
+      <div key={ album.collectionId }>
+        <p>{album.artistName}</p>
+        <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+        <div>
+          <h2>{album.collectionName}</h2>
+          <p>{album.artistName}</p>
+        </div>
+        <Link
+          data-testid={ `link-to-album-${album.collectionId}` }
+          to={ `/album/${album.collectionId}` }
+        />
+      </div>
+    ));
+  }
+
+  // populateAlbums = () => {
+  //   const { artist } = this.state;
+  //   return (
+  //     <section>
+  //       <h1>{ `Resultado de álbuns de: ${artist}` }</h1>
+  //       {this.renderAlbums()}
+  //     </section>);
+  // }
+
+  // populateAlbums = () => {
+  //   const { albumsResult, artist } = this.state;
+  //   // console.log(albumsResult);
+  //   // albumsResult.map((album) => console.log(album.artistName));
+  //   // if (albumsResult.length === 0) return <p>Nenhum álbum foi encontrado</p>;
+  //   // <h1>{ `Resultado de álbuns de: ${artist}` }</h1>;
+  //   const albums = albumsResult.map((album) => (
+  //     <div key={ album.collectionId }>
+  //       <Link
+  //         data-testid={ `link-to-album-${album.collectionId}` }
+  //         to={ `/album/${album.collectionId}` }
+  //       />
+  //       <p>{album.artistName}</p>
+  //       <img src={ album.artworkUrl100 } alt={ album.collectionName } />
+  //       <div>
+  //         <h2>{album.collectionName}</h2>
+  //         <p>{album.artistName}</p>
+  //       </div>
+  //     </div>
+  //   ));
+  // }
+
+  render() {
+    const { artist, searchResults, artistSearched } = this.state;
     const minLenght = 2;
+    const albums = this.renderAlbums();
     return (
       <Fragment>
         <Header />
@@ -33,10 +110,22 @@ class Search extends React.Component {
             data-testid="search-artist-button"
             type="submit"
             disabled={ artist.length < minLenght }
-            // onClick={ this.inputHandleChange }
+            onClick={ this.searchArtists }
           >
             Pesquisar
           </button>
+          <div className="search-preview">
+            { albums.length !== 0
+              // && <p>{ `Resultado de álbuns de: ${artist}` }</p> }
+              && <p>{ `Resultado de álbuns de: ${artistSearched}` }</p> }
+            {/* {albums.length === 0 && <p>Nenhum álbum foi encontrado</p>} */}
+            {(albums.length === 0 && searchResults) && <p>Nenhum álbum foi encontrado</p>}
+            { albums }
+          </div>
+          {/* { this.populateAlbums()} */}
+          {/* {(albums.length > 0
+            ? this.populateAlbums()
+            : artist.length > minLenght && <p>Nenhum álbum foi encontrado</p>)} */}
         </div>
       </Fragment>
     );
